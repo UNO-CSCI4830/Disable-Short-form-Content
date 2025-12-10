28f56219b02851c42e793c3c34139a63ba0d39bc
blazinchicken <kyblazinchicken@gmail.com>
2025-12-10 16:20:07 -0600

    again

--- shortform_tracker/settings.py
+++ shortform_tracker/settings.py
@@ -132,8 +132,11 @@ LOGIN_URL = '/accounts/login/'
 
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'tracker', 'assets'),  # 👈 points to your image folder
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
